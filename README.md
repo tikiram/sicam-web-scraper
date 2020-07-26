9360adedd4f1e38b3e38a326568fa3ce8ad1b37d
Tikiram  Ruiz Manuel <tikiram.ruiz@telusinternational.com>
2020-07-25 19:17:13 -0600

    update

---
+++ README.md
@@ -0,0 +1,20 @@
+
+# SICAM Scraper
+
+
+## Use
+```bash
+npm install
+```
+
+Create config file `scrapConfig.json` based on `scrapConfig-example.json`
+
+```bash
+node prueba.js
+```
+
+## Update
+
+```bash
+git pull origin master
+```
--- prueba.js
+++ prueba.js
@@ -1,23 +1,47 @@
-
 const fetch = require("node-fetch");
+const fs = require('fs');
+const util = require('util');
 const { parse } = require('node-html-parser');
+const scrapConfig = require('./scrapConfig.json');
+
+const readFile = util.promisify(fs.readFile);
+
+async function getValueFromFile(filename) {
+  const buffer = await readFile(filename);
+  return buffer.toString();
+}
+
+async function getValueFromPage (uri) {
+  const result = await fetch(uri);
+  return await result.text();
+}
 
 
+async function readDevices({ devices, sourceAction }) {
+  const resultPromises = devices.map(async (device) => {
+    const text = await sourceAction(device.uri);
+    const root = parse(text);
+    const tds = root.querySelectorAll('td');
+
+    const values = device.fields.map(field => {
+      const titleIndex = tds.findIndex(td => td.text.trim() === field);
+      const value = tds[titleIndex + 1].text;
+      return { field, value };
+    })
+
+    return {
+      name: device.name,
+      uri: device.uri,
+      values: values,
+    };
+  });
+
+  const results = await Promise.all(resultPromises);
+  console.log(JSON.stringify(results, null, 4));
+}
 
 
-// const getTextFrom
-
-const sendRequest = async () => {
-
-    const result = await fetch('https://google.com');
-    const text = await result.text();
-    console.log(text);
-    // const root = parse(text);
-    // console.log(root.querySelector('div'))
-};
-
-	sendRequest();
-
-// fetch('https://google.com')
-//   .then(response => response.text())
-//   .then(body => console.log(body));
+readDevices({
+  devices: scrapConfig.devices,
+  sourceAction: scrapConfig.network? getValueFromPage: getValueFromFile,
+});

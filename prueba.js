774b7b30506ed270300d954bd872b8834e276b5e
Tikiram Ruiz <tikiram.ruiz@gmail.com>
2023-01-01 17:41:59 -0600

    feat: result saved to file

--- prueba.js
+++ prueba.js
@@ -1,23 +1,46 @@
 const fetch = require("node-fetch");
-const fs = require('fs');
-const util = require('util');
-const { parse } = require('node-html-parser');
+const moment = require('moment');
+const fs = require('fs').promises;
+const {parse} = require('node-html-parser');
 const scrapConfig = require('./scrapConfig.json');
 
-const readFile = util.promisify(fs.readFile);
 
 async function getValueFromFile(filename) {
-  const buffer = await readFile(filename);
+  const buffer = await fs.readFile(filename);
   return buffer.toString();
 }
 
-async function getValueFromPage (uri) {
+async function getValueFromPage(uri) {
   const result = await fetch(uri);
   return await result.text();
 }
 
+function resultToString({name, uri, values}) {
+  const valuesText = values.map(({field, value}) => `${field}: ${value}`).join('\n');
 
-async function readDevices({ devices, sourceAction }) {
+  return `
+name: ${name}
+uri: ${uri}
+${valuesText}
+    `.trim();
+}
+
+async function writeResults(results) {
+  const finalString = results.map(resultToString).join('\n\n');
+
+  const timestamp = moment().format(scrapConfig.timeFormat);
+
+  const fileName = scrapConfig.output.replaceAll('$TIME', timestamp);
+
+  await fs.writeFile(fileName, finalString);
+
+  console.log(finalString);
+  console.log();
+  console.log('>>>> File saved: ' + fileName);
+}
+
+
+async function readDevices({devices, sourceAction}) {
   const resultPromises = devices.map(async (device) => {
     const text = await sourceAction(device.uri);
     const root = parse(text);
@@ -26,7 +49,7 @@ async function readDevices({ devices, sourceAction }) {
     const values = device.fields.map(field => {
       const titleIndex = tds.findIndex(td => td.text.trim() === field);
       const value = tds[titleIndex + 1].text;
-      return { field, value };
+      return {field, value};
     })
 
     return {
@@ -37,11 +60,20 @@ async function readDevices({ devices, sourceAction }) {
   });
 
   const results = await Promise.all(resultPromises);
-  console.log(JSON.stringify(results, null, 4));
+
+  await writeResults(results);
 }
 
+async function main() {
+  try {
+    await readDevices({
+      devices: scrapConfig.devices,
+      sourceAction: scrapConfig.network ? getValueFromPage : getValueFromFile,
+    });
+  } catch (error) {
+    console.log('An error happened:')
+    console.log(error);
+  }
+}
 
-readDevices({
-  devices: scrapConfig.devices,
-  sourceAction: scrapConfig.network? getValueFromPage: getValueFromFile,
-});
+void main();
